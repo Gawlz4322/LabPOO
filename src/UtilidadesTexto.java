a9c16c49984f36df6a9a2035e814605322e53306
Gawlz4322 <roberttries0411@gmail.com>
2025-08-29 09:20:13 -0400

    Taller01: private void menu() movido hacia arriba para mejor lectura

--- src/UtilidadesTexto.java
+++ src/UtilidadesTexto.java
@@ -3,14 +3,13 @@ public class UtilidadesTexto {
     public static void main(String[] args) {
         menu();
     }
-    public boolean VerificarPalindromo(String palindromo){
-        palindromo = palindromo.replaceAll("\\s+", "").toLowerCase();
-        String PalindromoRevertido = new StringBuilder(palindromo).reverse().toString();
-        return palindromo.equals(PalindromoRevertido);
-
-
-    }
     private void menu(){
         System.out.println("Menu: \n");
         System.out.println();
     }
+    public boolean VerificarPalindromo(String palindromo){
+        palindromo = palindromo.replaceAll("\\s+", "").toLowerCase();
+        String PalindromoRevertido = new StringBuilder(palindromo).reverse().toString();
+        return palindromo.equals(PalindromoRevertido);
+    }
+
