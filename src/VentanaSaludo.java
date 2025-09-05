fb10997905bfa398e81d35e89ea98be3ec6c8d7d
Gawlz4322 <roberttries0411@gmail.com>
2025-09-04 21:46:45 -0400

    Se añaden fuentes y se cambia el color de fondo, además de modificar el tamaño de ventana

--- src/VentanaSaludo.java
+++ src/VentanaSaludo.java
@@ -7,12 +7,21 @@ public class VentanaSaludo extends JFrame {
         ventana.setSize (500 , 350);
         ventana.setDefaultCloseOperation ( JFrame . EXIT_ON_CLOSE );
         ventana.setLayout ( null ) ;
+
+        ventana.getContentPane().setBackground(new Color(150, 170, 190));
+
         JTextField campoTexto = new JTextField () ;
         campoTexto.setBounds (50 , 50 , 200 , 25);
-        JButton botonSaludar = new JButton (" Saludar ") ;
+        JButton botonSaludar = new JButton (" Saludar ");
+        campoTexto.setFont(new Font("Roboto", Font.BOLD, 15));
+
         botonSaludar.setBounds(270,50 ,100,25);
+        botonSaludar.setFont (new Font("Roboto", Font.BOLD, 15));
+
         JLabel etiquetaSaludo = new JLabel ("") ;
         etiquetaSaludo.setBounds(50,80,300,25);
+        etiquetaSaludo.setFont(new Font("Roboto", Font.BOLD, 15));
+
         botonSaludar . addActionListener ( e -> {
             String nombre = campoTexto.getText () ;
             etiquetaSaludo.setText ("Hola ," + nombre ) ;
@@ -22,7 +31,8 @@ public class VentanaSaludo extends JFrame {
         ventana.add(etiquetaSaludo);
         ventana.setLocationRelativeTo(null);
         ventana.setVisible(true);
-
+    }
+    private static void aplicarEstilo() {
     }
 
 }
