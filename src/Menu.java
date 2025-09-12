fa91c4ac3d416cd714d2c0b6270b3126458b06ff
Gawlz4322 <roberttries0411@gmail.com>
2025-09-12 09:24:51 -0300

    Creacion ventana menu, añadido botones

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Menu {
+    private final JFrame frame = new JFrame("Ruleta - Casino Black Cat");
+    private final JButton btnInicio = new JButton("Inicio");
+    private final JButton btnJugar = new JButton("Jugar");
+    private final JButton btnHistorial = new JButton("Historial");
+
+    public  Menu() {
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+
+        frame.add(btnInicio);
+        frame.add(btnJugar);
+        frame.add(btnHistorial);
+    }
 }
