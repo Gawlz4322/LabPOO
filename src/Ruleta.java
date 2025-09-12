57fcc6146c9438262b9bddb7d034a4520ff2a571
Gawlz4322 <roberttries0411@gmail.com>
2025-09-12 09:19:04 -0300

    Se llama a login al iniciar, se crea clase menu

--- src/Ruleta.java
+++ src/Ruleta.java
@@ -11,7 +11,7 @@ public class Ruleta {
             {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36};
 
     public static void main(String[] args) {
-        menu();
+        new VentanaLoginRuleta();
     }
     /**
      * Llama a menú.
