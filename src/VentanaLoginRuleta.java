91803953e0839779953a306e94e7eaa56d082422
Gawlz4322 <roberttries0411@gmail.com>
2025-09-12 09:05:55 -0300

    Importando codigo de login de branch. Parte 1

--- src/VentanaLoginRuleta.java
+++ src/VentanaLoginRuleta.java
@@ -3,5 +3,39 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class VentanaLoginRuleta {
+public class VentanaLoginRuleta {    public static final List<Usuarios> USUARIOS = new ArrayList<>();
+
+    private final JFrame frame = new JFrame("Login - Casino Black Cat");
+    private final JLabel lblUsuario = new JLabel("Usuario:");
+    private final JTextField txtUsuario = new JTextField();
+    private final JLabel lblClave = new JLabel("Clave:");
+    private final JPasswordField txtClave = new JPasswordField();
+    private final JButton btnIngresar = new JButton("Ingresar");
+
+    public  VentanaLogin() {
+        USUARIOS.add(new Usuarios("admin", "1234", "Administrador"));
+        USUARIOS.add(new Usuarios("Juanin", "abcd", "Juanin Juan Harry"));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        frame.setLayout(null);
+
+        frame.add(lblUsuario);
+        frame.add(txtUsuario);
+        frame.add(lblClave);
+        frame.add(txtClave);
+        frame.add(btnIngresar);
+
+        lblUsuario.setBounds(50, 80,300,25);
+        txtUsuario.setBounds(50, 120,300,25);
+        lblClave.setBounds(50, 160,300,25);
+        txtClave.setBounds(50, 200,300,25);
+        btnIngresar.setBounds(50, 240,300,25);
+
+        btnIngresar.addActionListener(e -> login());
+
+        mostrarVentana();
+
+    }
+
 }
