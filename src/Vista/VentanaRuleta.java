6b253753d406e7158f74237da2aeca3a595e348b
Gawlz4322 <roberttries0411@gmail.com>
2025-09-26 09:44:52 -0300

    Se crea metodo para iniciar la VentanaRuleta

--- src/Vista/VentanaRuleta.java
+++ src/Vista/VentanaRuleta.java
@@ -6,8 +6,15 @@ import java.awt.*;
 
 public class VentanaRuleta {
     private final JFrame frame = new JFrame("Ruleta - Casino Black Cat");
-    public VentanaRuleta() {
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private final Ruleta ruleta;
+
+    public VentanaRuleta(Ruleta ruleta) {
+        this.ruleta = ruleta;
+        iniciarVentanaRuleta();
+    }
+    private void iniciarVentanaRuleta() {
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setLayout(new BorderLayout());
         frame.setSize(800, 600);
         frame.setResizable(false);
