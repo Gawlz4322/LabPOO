83113262eaef8ad38e5c73f0fe86c0b017d8b7cf
Gawlz4322 <roberttries0411@gmail.com>
2025-09-26 09:35:21 -0300

    Se crea frame de VentanaRuleta

--- src/Vista/VentanaRuleta.java
+++ src/Vista/VentanaRuleta.java
@@ -1,4 +1,16 @@
 package Vista;
 
+import Modelo.Ruleta;
+import javax.swing.*;
+import java.awt.*;
+
 public class VentanaRuleta {
+    private final JFrame frame = new JFrame("Ruleta - Casino Black Cat");
+    public VentanaRuleta() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.setSize(800, 600);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+    }
 }
