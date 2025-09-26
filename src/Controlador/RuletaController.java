69415dfdcd408863357ece8399c45c4ee0b7dd1a
Gawlz4322 <roberttries0411@gmail.com>
2025-09-26 10:09:38 -0300

    Se importan clases de modelo y libreria. Se crea metodo RuletaController

--- src/Controlador/RuletaController.java
+++ src/Controlador/RuletaController.java
@@ -1,4 +1,17 @@
 package Controlador;
 
+import Modelo.Ruleta;
+import Modelo.TipoApuesta;
+import Vista.VentanaRuleta;
+
+import javax.swing.*;
+
 public class RuletaController {
+    public Ruleta ruleta;
+    public VentanaRuleta ventanaRuleta;
+
+    public RuletaController(Ruleta ruleta, VentanaRuleta vista) {
+        this.ruleta = ruleta;
+        this.ventanaRuleta = vista;
+    }
 }
