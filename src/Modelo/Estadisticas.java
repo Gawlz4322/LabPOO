beacd10b10c468b3663fe7e83b980493598f3885
Gawlz4322 <roberttries0411@gmail.com>
2025-10-10 10:24:02 -0300

    Clase Estadisticas añadida, a partir de VisualParadigm

---
+++ src/Modelo/Estadisticas.java
@@ -0,0 +1,23 @@
+package Modelo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Estadisticas {
+	public int totalJugadas;
+	public int victorias;
+	public double porcentajeVictorias;
+	public int rachaMaxima;
+	public TipoApuesta tipoMasJugado;
+    public Estadisticas() {}
+
+	public int calcularTotalJugadas(List<Resultado> historial) {
+    }
+
+    public int calcularVictorias(List<Resultado> historial) {
+    }
+
+    public TipoApuesta calcularTipoMasJugado(List<Resultado> historial) {
+    }
+}
