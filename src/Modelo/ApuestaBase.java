3b70bc691646760a1c697ef37ad439d32309a10b
Gawlz4322 <roberttries0411@gmail.com>
2025-10-24 09:51:21 -0300

    Se completa el constructor

--- src/Modelo/ApuestaBase.java
+++ src/Modelo/ApuestaBase.java
@@ -1,5 +1,11 @@
 package Modelo;
 abstract public class ApuestaBase{
-    public ApuestaBase(){}
+    private final int montoApostado;
+    protected String etiqueta;
+
+    public ApuestaBase(int montoApostado, String etiqueta){
+        this.montoApostado = montoApostado;
+        this.etiqueta = etiqueta;
+    }
     public void acierta(int numero, String color){}
 }
