095ecca7546d7eb4eb3b2eba19f12ca6f55e607e
Gawlz4322 <roberttries0411@gmail.com>
2025-10-29 20:44:49 -0300

    Constructor inicial

--- src/ColeccionVinilos/CollecionVinilos.java
+++ src/ColeccionVinilos/CollecionVinilos.java
@@ -1,4 +1,10 @@
 package ColeccionVinilos;
+import java.util.ArrayList;
 
 public class CollecionVinilos {
+    private final int cantidadMax = 100;
+    private ArrayList<Vinilo> collecion;
+    public CollecionVinilos(){
+        collecion = new ArrayList<>();
+    }
 }
