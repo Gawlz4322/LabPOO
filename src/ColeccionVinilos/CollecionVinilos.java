4c5a93eba0d5a2451d45e10c1fd5238cf9d430e4
Gawlz4322 <roberttries0411@gmail.com>
2025-10-29 20:59:26 -0300

    Se renombra variable para evitar confusion

--- src/ColeccionVinilos/CollecionVinilos.java
+++ src/ColeccionVinilos/CollecionVinilos.java
@@ -2,13 +2,13 @@ package ColeccionVinilos;
 import java.util.ArrayList;
 
 public class CollecionVinilos {
-    private final int cantidadMax = 100;
+    private final int capacidadMaxima = 100;
     private ArrayList<Vinilo> collecion;
     public CollecionVinilos(){
         collecion = new ArrayList<>();
     }
-    public int getCantidadMax() {
-        return cantidadMax;
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
     }
     public int totalVinilos(){
         return collecion.size();
