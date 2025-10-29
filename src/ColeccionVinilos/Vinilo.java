41bd8ed66726ee77d90d6eabd34b4711557ff022
Gawlz4322 <roberttries0411@gmail.com>
2025-10-29 20:42:30 -0300

    Constructor inicial

--- src/ColeccionVinilos/Vinilo.java
+++ src/ColeccionVinilos/Vinilo.java
@@ -1,4 +1,12 @@
 package ColeccionVinilos;
 
 public class Vinilo {
+    private String artista;
+    private String album;
+    public int año;
+    public Vinilo(String artista, String album, int año) {
+        this.artista = artista;
+        this.album = album;
+        this.año = año;
+    }
 }
