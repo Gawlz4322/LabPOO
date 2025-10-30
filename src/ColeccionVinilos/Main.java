51feba43f7c6c20187d288d0d6147db1088ecca6
Gawlz4322 <roberttries0411@gmail.com>
2025-10-29 21:08:29 -0300

    Corrección

--- src/ColeccionVinilos/Main.java
+++ src/ColeccionVinilos/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         ColecionVinilos coleccion = new ColecionVinilos();
 
-        agregarVinilo("Iron Maiden", "Iron Maiden", 1980);
-        agregarVinilo("Iron Maiden", "Killers", 1981);
-        agregarVinilo("Iron Maiden", "The number of the beast", 1982);
-        agregarVinilo("AC-DC", "Back in black", 1980);
-        agregarVinilo("AC-DC", "Highway to Hell", 1979);
-        agregarVinilo("AC-DC", "Who made who", 1986);
-        agregarVinilo("Judas Priest", "British steel", 1980);
-        agregarVinilo("Judas Priest", "Painkiller", 1990);
-        agregarVinilo("Judas Priest", "Defenders of the faith", 1984);
-        agregarVinilo("Kiss", "Destroyer", 1976);
+        coleccion.agregarVinilo("Iron Maiden", "Iron Maiden", 1980);
+        coleccion.agregarVinilo("Iron Maiden", "Killers", 1981);
+        coleccion.agregarVinilo("Iron Maiden", "The number of the beast", 1982);
+        coleccion.agregarVinilo("AC-DC", "Back in black", 1980);
+        coleccion.agregarVinilo("AC-DC", "Highway to Hell", 1979);
+        coleccion.agregarVinilo("AC-DC", "Who made who", 1986);
+        coleccion.agregarVinilo("Judas Priest", "British steel", 1980);
+        coleccion.agregarVinilo("Judas Priest", "Painkiller", 1990);
+        coleccion.agregarVinilo("Judas Priest", "Defenders of the faith", 1984);
+        coleccion.agregarVinilo("Kiss", "Destroyer", 1976);
 
         System.out.println("Espacio máximo colleción:" + coleccion.getCapacidadMaxima());
         System.out.println("Hay un total de:" + coleccion.totalVinilos() + "vinilos en la colleción.");
