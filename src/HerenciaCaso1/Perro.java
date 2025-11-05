ee43d48b6988301888113e3daf621001d2e75cf5
Gawlz4322 <roberttries0411@gmail.com>
2025-11-05 19:21:08 -0300

    Método para ladrar

--- src/HerenciaCaso1/Perro.java
+++ src/HerenciaCaso1/Perro.java
@@ -5,4 +5,7 @@ public class Perro extends Animal{
     public Perro(String nombre, String color, int edad) {
         super(nombre, color, edad);
     }
+    public void ladrar(){
+        System.out.println(getNombre() + "Guau guau guau.");
+    }
 }
