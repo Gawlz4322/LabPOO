1d756381fcbb521d61501c44c37148ab93fcb417
Gawlz4322 <roberttries0411@gmail.com>
2025-11-05 19:19:47 -0300

    Constructores iniciales de herencia

--- src/HerenciaCaso1/Gato.java
+++ src/HerenciaCaso1/Gato.java
@@ -1,4 +1,7 @@
 package HerenciaCaso1;
 
 public class Gato extends Animal{
+    public Gato(String nombre, String color, int edad) {
+        super(nombre, color, edad);
+    }
 }
--- src/HerenciaCaso1/Perro.java
+++ src/HerenciaCaso1/Perro.java
@@ -1,4 +1,8 @@
 package HerenciaCaso1;
 
 public class Perro extends Animal{
+
+    public Perro(String nombre, String color, int edad) {
+        super(nombre, color, edad);
+    }
 }
