3cfd058dbfdbecd337357bbe3e729c1fe957fda9
Gawlz4322 <roberttries0411@gmail.com>
2025-11-05 20:55:04 -0300

    Constructor inicial

--- src/HerenciaCaso2/GrupoProyecto.java
+++ src/HerenciaCaso2/GrupoProyecto.java
@@ -1,4 +1,13 @@
 package HerenciaCaso2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GrupoProyecto {
+    private String nombre;
+    private List<Empleado> miembros;
+    public  GrupoProyecto(String nombre) {
+        this.nombre = nombre;
+        this.miembros = new ArrayList<>();
+    }
 }
