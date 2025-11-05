509460f56c22456255993eabaa231bbf13d70c59
Gawlz4322 <roberttries0411@gmail.com>
2025-11-05 20:01:00 -0300

    Clase vehículo con constructor inicial y atributos

---
+++ src/HerenciaCaso2/Vehículo.java
@@ -0,0 +1,10 @@
+package HerenciaCaso2;
+
+public class Vehículo {
+    private String marca;
+    private String modelo;
+    public Vehículo(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+}
