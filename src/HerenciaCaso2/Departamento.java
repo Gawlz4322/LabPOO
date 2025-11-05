76e6367ad89719187f2c0a7a8f8878a56ddd0bfd
Gawlz4322 <roberttries0411@gmail.com>
2025-11-05 20:52:34 -0300

    Creación de grupos de proyecto (composición con departamento)

--- src/HerenciaCaso2/Departamento.java
+++ src/HerenciaCaso2/Departamento.java
@@ -1,8 +1,13 @@
 package HerenciaCaso2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Departamento {
     private String nombre;
+    private List<GrupoProyecto> grupos;
     public Departamento(String nombre) {
         this.nombre = nombre;
+        this.grupos = new ArrayList<>();
     }
 }
