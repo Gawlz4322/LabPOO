6802e500dea015cfacb909c359df241feeb68004
Gawlz4322 <roberttries0411@gmail.com>
2025-11-05 19:05:30 -0300

    Constructor inicial + herencia

--- src/CasosHerencia/Persona.java
+++ src/CasosHerencia/Persona.java
@@ -1,4 +1,9 @@
 package CasosHerencia;
 
-public class Persona {
+public class Persona extends SerVivo{
+    private String nombre;
+    public Persona(String nombre, int edad) {
+        super(edad);
+        this.nombre = nombre;
+    }
 }
