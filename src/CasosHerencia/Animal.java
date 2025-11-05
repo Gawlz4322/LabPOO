990dcf566ef17d68056b86c4ece2b73ee310c8fa
Gawlz4322 <roberttries0411@gmail.com>
2025-11-05 19:09:47 -0300

    Constructor inicial + atributos necesarios

--- src/CasosHerencia/Animal.java
+++ src/CasosHerencia/Animal.java
@@ -1,5 +1,11 @@
 package CasosHerencia;
 
 public class Animal extends SerVivo{
-
+    private String nombre;
+    private String color;
+    public Animal(String nombre, String color, int edad) {
+        super(edad);
+        this.nombre = nombre;
+        this.color = color;
+    }
 }
