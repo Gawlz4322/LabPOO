78299f97b6917eca2efa3f5088cd2d229d4a3f3e
Gawlz4322 <roberttries0411@gmail.com>
2025-11-05 20:07:15 -0300

    Clase empresa naviera, heredada de Empresa con constructor heredado y departamentos

---
+++ src/HerenciaCaso2/EmpresaTesoros.java
@@ -0,0 +1,18 @@
+package HerenciaCaso2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpresaTesoros extends Empresa {
+    private List<Departamento> departamentos;
+    public EmpresaTesoros(String nombre) {
+        super(nombre);
+        this.departamentos = new ArrayList<>();
+        this.departamentos.add(new Departamento("Adquisiciones"));
+        this.departamentos.add(new Departamento("Ventas"));
+        this.departamentos.add(new Departamento("Producción"));
+        this.departamentos.add(new Departamento("Logística"));
+        this.departamentos.add(new Departamento("Finanzas"));
+    }
+
+}
