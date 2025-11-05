37fa2c44f5d713d99ef8d8f6b2baf7541995ba5d
Gawlz4322 <roberttries0411@gmail.com>
2025-11-05 19:22:17 -0300

    Método para rasguñar

--- src/HerenciaCaso1/Gato.java
+++ src/HerenciaCaso1/Gato.java
@@ -4,4 +4,7 @@ public class Gato extends Animal{
     public Gato(String nombre, String color, int edad) {
         super(nombre, color, edad);
     }
+    public void rasguñar(){
+        System.out.println(getNombre()+ " rasguña.");
+    }
 }
