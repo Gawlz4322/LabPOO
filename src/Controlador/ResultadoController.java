477747389e620a9293a4511fe53bf93ebeaaa355
Gawlz4322 <roberttries0411@gmail.com>
2025-11-13 23:52:00 -0300

    Se le da funcionalidad a la clase ResultadoController

--- src/Controlador/ResultadoController.java
+++ src/Controlador/ResultadoController.java
@@ -1,4 +1,19 @@
 package Controlador;
 
+import Modelo.IRepositorioResultados;
+import Modelo.Resultado;
+import Vista.VentanaHistorial;
+import java.util.List;
+
 public class ResultadoController {
+
+    private final VentanaHistorial vista;
+    private final IRepositorioResultados repositorio;
+
+    public ResultadoController(VentanaHistorial vista, IRepositorioResultados repositorio) {
+        this.vista = vista;
+        this.repositorio = repositorio;
+        List<Resultado> historial = this.repositorio.obtenerHistorial();
+        this.vista.mostrarHistorial(historial);
+    }
 }
