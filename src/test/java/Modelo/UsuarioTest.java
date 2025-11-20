4c711cb09d2f003013ad0ad68df67e475172b4a6
Gawlz4322 <roberttries0411@gmail.com>
2025-11-19 21:24:42 -0300

    Test para ver si un deposito válido incrementa el saldo

--- src/test/java/Modelo/UsuarioTest.java
+++ src/test/java/Modelo/UsuarioTest.java
@@ -13,5 +13,10 @@ class UsuarioTest {
         });
         assertEquals("Saldo inicial inválido", exception.getMessage());
     }
-
+    @Test
+    void testDepositoValido() {
+        Usuario usuario = new Usuario("juan", "1234", "Juan Perez", 1000.0);
+        usuario.cargarSaldo(500.0);
+        assertEquals(1500.0, usuario.getSaldo(), "El saldo debería sumar el depósito");
+    }
 }
