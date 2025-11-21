57fb4a7d1075069c09ac88e56327a0364bd99359
Gawlz4322 <roberttries0411@gmail.com>
2025-11-21 09:40:11 -0300

    Constructor inicial

--- src/main/java/Controlador/RegistroController.java
+++ src/main/java/Controlador/RegistroController.java
@@ -1,4 +1,13 @@
 package Controlador;
+import Vista.VentanaRegistro;
+import javax.swing.JOptionPane;
 
 public class RegistroController {
+    private final VentanaRegistro vista;
+    private final SessionController session;
+
+    public RegistroController(VentanaRegistro vista, SessionController session) {
+        this.vista = vista;
+        this.session = session;
+    }
 }
