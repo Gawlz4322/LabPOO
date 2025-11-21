7e76b8867faebe9191f357890f09cf0957ebc0fe
Gawlz4322 <roberttries0411@gmail.com>
2025-11-21 09:49:29 -0300

    Se asegura de que las llamadas iniciales esten protegidas

--- src/main/java/Launcher/Launcher.java
+++ src/main/java/Launcher/Launcher.java
@@ -2,7 +2,9 @@ package Launcher;
 
 import Controlador.SessionController;
 import Vista.VentanaLoginRuleta;
-import javax.swing.SwingUtilities;
+
+import javax.swing.*;
+
 import Modelo.IRepositorioResultados;
 import Modelo.RepositorioEnMemoria;
 
@@ -10,9 +12,13 @@ public class Launcher {
     static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             SessionController session = new SessionController();
-            session.registrarUsuario("admin", "1234", "Administrador");
-            session.registrarUsuario("Juanin", "abcd", "Juanin Harry");
             IRepositorioResultados repositorio = new RepositorioEnMemoria();
+            try {
+                session.registrarUsuario("admin", "1234", "Administrador");
+                session.registrarUsuario("Juanin", "abcd", "Juanin Harry");
+            }catch(IllegalArgumentException e){
+                System.out.println("Advertencia: " + e.getMessage());
+            }
             new VentanaLoginRuleta(session, repositorio).mostrarVentana();
         });
     }
